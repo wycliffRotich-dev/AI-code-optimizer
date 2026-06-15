53235f7ac5eb0869f1ce927293db4bb43c4547d0
WYCLIFFEROTICH-Dev <wycliffeone6@gmail.com>
2026-06-15 18:13:46 +0300

    feat: add ad component

---
+++ app/components/AdBanner.tsx
@@ -0,0 +1,7 @@
+import Script from 'next/script'; 
+ 
+export default function AdBanner() { 
+  return ( 
+    <div className="flex justify-center my-4"><div id="container-f380c274d1ab56acfcda1e4462a4bf77"></div><Script id="cpm-banner-script" strategy="afterInteractive" src="https://pl29755219.effectivecpmnetwork.com/f3/80/c2/f380c274d1ab56acfcda1e4462a4bf77.js" /></div>    ); 
+  ); 
+} 
